978330a728cde144e0e2ee5f5cab2d27f560679c
dean6969 <deanblog.info@gmail.com>
2023-06-28 01:06:45 +1000

    fix lại backend lỗi thư viện

--- Server/app.py
+++ Server/app.py
@@ -2,14 +2,14 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from peft import PeftModel, PeftConfig
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from flaskext.mysql import MySQL #pip install flask-mysql
-import pymysql
-model_path = "./bloomz-lora-vi-chatbot/"
+import torch
+import re
+model_path = "./bloomz-lora-vi-chatbot/bloomz-lora-en-ecommerce"
 config = PeftConfig.from_pretrained(model_path)
 
 # load base LLM model and tokenizer
 tokenizer_from_file = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
-model_from_file = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path).to("cuda")
+model_from_file = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path)
 
 
 # configuration
@@ -39,16 +39,18 @@ def receive_string():
     # Do biến inputs được lưu trên cpu mà model thì load trên gpu nên phải chuyển
     # biến này lên gpu bằng hàm .to("cuda")
     data = {}
-    with torch.autocast("cuda"):
-        outputs = model_from_file.generate(input_ids=inputs["input_ids"].to("cuda"),
-                                max_new_tokens=100,
-                                no_repeat_ngram_size=3,
-                                num_beams=3,
-                                num_return_sequences=3)
-        count = 1
-        for i in tokenizer_from_file.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True):
-            print(i)
-            data[count] = i
-            count += 1
+    outputs = model_from_file.generate(input_ids=inputs["input_ids"],
+                            max_new_tokens=100,
+                            no_repeat_ngram_size=3,
+                            num_beams=3,
+                            num_return_sequences=1)
     
+    answer = tokenizer_from_file.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0]
+    
+    answer = re.findall(r"(?<=Đáp: ).*", answer)[0]
+    
+    data = {"answer": answer}
     return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
